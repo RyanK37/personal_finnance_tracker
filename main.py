21797aa4a31ba37e1dd8410e45e242f50169ec71
Ryan <hein003326@gmail.com>
2024-04-13 02:44:43 +0100

    modify filter funciton

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ username_entry.grid(row=1, column=1)
 password_entry = tk.Entry(start_window, show="*", font=("calibri", 14))
 password_entry.grid(row=2, column=1)
 
+# ===============================================================================================================================
+
 def update_balance_label():
     balance.config(text="Current balance : " + str(account_balance))
     
@@ -32,14 +34,24 @@ def update_balance_label():
 def update_last_tran():
     last_tran_label.config(text="Last Transation : " + str(tran_data[-1]))
     
-# def update_filter_tran():
-#     show_tran.config(text="Filtered Transations : " + str(tran_data))
-    
-    
+# ===============================================================================================================================
+
+def filter_by_input(start, end, type, category, source):
+        start_date = pd.to_datetime(start, format="%d-%b-%Y")
+        end_date = pd.to_datetime(end, format="%d-%b-%Y")
+        
+        filtered_list = []
+        for each_data in tran_data:
+            if start_date <= pd.to_datetime(each_data["date"], format="%d-%b-%Y") <= end_date and\
+                each_data["type"] == type and\
+                each_data["category"] == category and\
+                each_data["payee/source"].lower() == source.lower():
+                filtered_list.append(each_data)
+        return filtered_list
+
 # ===============================================================================================================================
     
 def pie_chart():
-
     income_list = [data for data in tran_data if data["type"] == "Income"]
     expense_list = [data for data in tran_data if data["type"] == "Expense"]
     
@@ -67,25 +79,60 @@ def pie_chart():
     plt.suptitle("Income and Expense Pie Charts", fontsize=16, fontname ="calibri")
     plt.show()
 
+# ===============================================================================================================================
+def piechart():
+    piechart_window = tk.Tk()
+    piechart_window.geometry("500x400")
+    piechart_window.title("Pie Chart")    
     
+    date_frame = tk.Frame(piechart_window)
+    date_frame.pack()
     
+    date_range_from_label = tk.Label(date_frame, text="Time Range(From)", font=("calibri", 14))
+    date_range_from_label.grid(row=1, column=0)
+    date_range_from = tk.Entry(date_frame)
+    date_range_from.grid(row=1, column=1)
+    
+    date_range_to_label = tk.Label(date_frame, text="Time Range(To)", font=("calibri", 14))
+    date_range_to_label.grid(row=2, column=0)
+    date_range_to = tk.Entry(date_frame)
+    date_range_to.grid(row=2, column=1)
+    
+    radio_frame = tk.Frame(piechart_window)
+    radio_frame.pack()
+    
+    choose_type = tk.StringVar(value="Income")
+    income_radio = tk.Radiobutton(radio_frame, text="Income", variable=choose_type, value="Income", font=("calibri", 14))
+    income_radio.pack(side="left")
+    expense_radio = tk.Radiobutton(radio_frame, text="Expense", variable=choose_type, value="Expense", font=("calibri", 14))
+    expense_radio.pack(side="right")
+    
+    categories = {"Income": ["Salary", "Pension", "Interest", "Others"],
+                  "Expense": ["Food", "Rent", "Clothing", "Car", "Health", "Others"]}
+    category_real = tk.StringVar(value="Salary")
+    
+    category_label =tk.Label(piechart_window, text="Category",  font=("calibri", 14))
+    category_label.pack()
+    category_menu = tk.OptionMenu(piechart_window, category_real, *categories["Income"] + categories["Expense"])
+    category_menu.pack()
+    
+    source_label = tk.Label(piechart_window, text="Payee/Source",  font=("calibri", 14))
+    source_label.pack()
+    source_entry = tk.Entry(piechart_window)
+    source_entry.pack()
+    
+    piechart_window.mainloop()
+    
+# ===============================================================================================================================
+
+
+
 # ===============================================================================================================================
 
 def summary():
     summary_window = tk.Tk()
     summary_window.geometry("700x400")
     summary_window.title("Dashboard")
-    
-    # global show_tran
-    # if len(tran_data) == 0 :
-    #     show_tran = tk.Label(summary_window, text="Empty transation", font=("calibri", 14))
-    #     show_tran.pack() 
-    # else:
-    #     for data in tran_data:
-    #         show_tran = tk.Label(summary_window, text=str(data), font=("calibri", 14))
-    #         show_tran.pack()
-        
-    # ---------------------------------------------------------------------------------
     
     date_frame = tk.Frame(summary_window)
     date_frame.pack()
@@ -123,28 +170,29 @@ def summary():
     source_entry = tk.Entry(summary_window)
     source_entry.pack()
     
-    
-    
     # ---------------------------------------------------------------------------------
-    def filter_by_input():
-        start_date = pd.to_datetime(date_range_from.get(), format="%d-%b-%Y")
-        end_date = pd.to_datetime(date_range_to.get(), format="%d-%b-%Y")
+    
+    def show_filtered_data():
+        filtered_data = filter_by_input(date_range_from.get(), date_range_to.get(), choose_type.get(), category_real.get(), source_entry.get())
         
-        filtered_list = []
-        for each_data in tran_data:
-            if start_date <= pd.to_datetime(each_data["date"], format="%d-%b-%Y") <= end_date and each_data["type"] == choose_type.get() and each_data["category"] == category_real.get() and each_data["payee/source"] == source_entry.get():
-                filtered_list.append(each_data)
-        
-        for each in filtered_list:
-            show_filter_tran = tk.Label(summary_window, text=str(each), font=("calibri", 14))
-            show_filter_tran.pack()
+        if filtered_data == []:
+            filtered_label = tk.Label(summary_window, text="There is no transation!!", font=("calibri", 14))
+            filtered_label.pack()
+        else:
+            for each in filtered_data:
+                filtered_label = tk.Label(summary_window, text=str(each), font=("calibri", 14))
+                filtered_label.pack()
             
-
-    show_btn = tk.Button(summary_window, text="Show", font=("calibri", 14), command=filter_by_input)
+    # ---------------------------------------------------------------------------------
+        
+    show_btn = tk.Button(summary_window, text="Show", font=("calibri", 14), command=show_filtered_data)
     show_btn.pack()
     
     # print_btn = tk.Button(summary_window, text="Print", font=("calibri", 14), command=)
     # print_btn.pack()
+    
+    # barchart_btn = tk.Button(summary_window, text="Bar Chart", font=("calibri", 14), command=)
+    # barchart_btn.pack()
     
     piechart_btn = tk.Button(summary_window, text="Pie Chart",  font=("calibri", 14), command=pie_chart)
     piechart_btn.pack()
@@ -180,6 +228,8 @@ def add_tran():
     add_window.geometry("500x500")
     add_window.title("Add Transations")
     
+    # ---------------------------------------------------------------------------------
+    
     radio_frame = tk.Frame(add_window)
     radio_frame.pack()
     
@@ -189,6 +239,8 @@ def add_tran():
     expense_radio = tk.Radiobutton(radio_frame, text="Expense", variable=choose_type, value="Expense", font=("calibri", 14))
     expense_radio.pack(side="right")
     
+    # ---------------------------------------------------------------------------------
+    
     categories = {"Income": ["Salary", "Pension", "Interest", "Others"],
                   "Expense": ["Food", "Rent", "Clothing", "Car", "Health", "Others"]}
     category_real = tk.StringVar(value="Salary")
@@ -197,7 +249,9 @@ def add_tran():
     category_label.pack()
     category_menu = tk.OptionMenu(add_window, category_real, *categories["Income"] + categories["Expense"])
     category_menu.pack()
-    
+       
+    # ---------------------------------------------------------------------------------
+     
     amount_label = tk.Label(add_window, text="Amount", font=("calibri", 14))
     amount_label.pack()
     amount_entry = tk.Entry(add_window)
@@ -212,9 +266,13 @@ def add_tran():
     source_label.pack()
     source_entry = tk.Entry(add_window)
     source_entry.pack()
+        
+    # ---------------------------------------------------------------------------------
     
     add_btn = tk.Button(add_window, text="Submit" , font=("calibri", 14), command=lambda : update_tran_data(choose_type.get(), category_real.get(), int(amount_entry.get()), date_entry.get(), source_entry.get()))
     add_btn.pack()
+        
+    # ---------------------------------------------------------------------------------
     
     add_window.mainloop()
     
